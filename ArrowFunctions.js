93a8ba80f888badb060e0af53ed756196ccf28ec
Kushagra420 <130694271+Kushagra420@users.noreply.github.com>
2025-07-04 11:28:32 +0530

    Add files via upload

---
+++ ArrowFunctions.js
@@ -0,0 +1,8 @@
+let getName=()=>{console.log("Arrow function output.")};
+let getId=()=>100;
+let searchCode=(id)=>id;
+
+getName();
+console.log(getId());
+console.log(searchCode(101));
+
---
+++ demo.js
@@ -0,0 +1,18 @@
+console.log("Javascript");
+
+// function getName(){
+//     console.log("get userName");
+// }
+
+function getUserId(id){
+    
+    const count=100;
+    console.log(count);
+    return id;
+    
+}
+
+// console.log(count) // out of the scope
+
+// getName();
+console.log(getUserId(101));
